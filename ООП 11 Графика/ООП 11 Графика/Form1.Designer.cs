8c5e1893f0265730e9c2f46b9891b4a3b6eef5d2
EgorFrom <egor.kush@bk.ru>
2017-12-01 16:49:34 +0500

    Work with PictureBox accomplished. Further additionally tasks

--- ООП 11 Графика/ООП 11 Графика/Form1.Designer.cs
+++ ООП 11 Графика/ООП 11 Графика/Form1.Designer.cs
@@ -52,6 +52,9 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -64,6 +67,9 @@
             this.tabPage3.SuspendLayout();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -82,6 +88,7 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
             this.tabPage1.Controls.Add(this.panel1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
@@ -139,6 +146,7 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.pictureBox2);
             this.tabPage2.Controls.Add(this.panel2);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
@@ -258,6 +266,7 @@
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.pictureBox3);
             this.tabPage3.Controls.Add(this.label5);
             this.tabPage3.Controls.Add(this.label6);
             this.tabPage3.Controls.Add(this.panel3);
@@ -268,6 +277,7 @@
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "tabPage3";
             this.tabPage3.UseVisualStyleBackColor = true;
+            this.tabPage3.SizeChanged += new System.EventHandler(this.tabPage3_SizeChanged);
             this.tabPage3.Paint += new System.Windows.Forms.PaintEventHandler(this.tabPage3_Paint);
             this.tabPage3.Leave += new System.EventHandler(this.tabPage3_Leave);
             // 
@@ -280,7 +290,7 @@
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.Location = new System.Drawing.Point(3, 3);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(442, 57);
+            this.panel3.Size = new System.Drawing.Size(442, 66);
             this.panel3.TabIndex = 0;
             // 
             // trackBar1
@@ -346,6 +356,37 @@
             this.label7.TabIndex = 5;
             this.label7.Text = "Изменение скорости";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(442, 287);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(311, 384);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox3.Location = new System.Drawing.Point(3, 66);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(442, 321);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox3_Paint);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -377,6 +418,9 @@
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -406,6 +450,9 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- ООП 11 Графика/ООП 11 Графика/Form1.cs
+++ ООП 11 Графика/ООП 11 Графика/Form1.cs
@@ -28,7 +28,7 @@ namespace ООП_11_Графика
         }
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
-            Refresh();
+           pictureBox1.Refresh();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -45,86 +45,11 @@ namespace ООП_11_Графика
         }
         void ColorComboBox_Changed(object sender, EventArgs e)
         {
-            Refresh();
+           pictureBox1.Refresh();
         }
         private void tabPage1_Paint(object sender, PaintEventArgs e)
         {
-            int y = 10;
-            Graphics g = e.Graphics;
-            Pen p = new Pen(Color.FromName(CB.SelectedItem.ToString()), 1);
-            Point beginXDown = new Point(0, (ClientSize.Height / 2 - 100) + (int)(numericUpDown1.Value / 2));
-            Point endXDown = new Point(ClientSize.Width, (ClientSize.Height / 2 - 100) + (int)(numericUpDown1.Value / 2));
-            Point beginXUp = new Point(0, (ClientSize.Height / 2 - 100) - (int)(numericUpDown1.Value / 2));
-            Point endXUp = new Point(ClientSize.Width, (ClientSize.Height / 2 - 100) - (int)(numericUpDown1.Value / 2));
-            g.DrawLine(p, beginXUp, endXUp);
-            g.DrawLine(p, beginXDown, endXDown);
-            //кадр
-            float widthCadr = (float)numericUpDown1.Value;
-            float HeightCadr = (float)numericUpDown1.Value * 2 / 3;
-            Point XCadr = new Point((int)(beginXUp.X + numericUpDown1.Value / 8), (int)(beginXUp.Y + numericUpDown1.Value / 6));
-            int CountRect = 0;
-            for (decimal i = 0; i < ClientSize.Width; i += numericUpDown1.Value + numericUpDown1.Value / 8)
-                if ((beginXUp.X + (float)numericUpDown1.Value / 8) + (float)i + (float)numericUpDown1.Value < ClientSize.Width - 10)
-                {
-                    g.DrawRectangle(p, (beginXUp.X + (float)numericUpDown1.Value / 8) + (float)i, (beginXUp.Y + (float)numericUpDown1.Value / 6), (float)numericUpDown1.Value, (float)numericUpDown1.Value * 2 / 3);
-                    CountRect++;
-                }
-            for (decimal i = 0; i < ClientSize.Width; i += numericUpDown1.Value / (decimal)2.1 )
-                g.DrawEllipse(p, beginXUp.X+ (float)(numericUpDown1.Value / 8) + (float)i, (beginXUp.Y + 2) , (float)numericUpDown1.Value / 8, (float)(numericUpDown1.Value / 8) );
-            for (decimal i = 0; i < ClientSize.Width; i += numericUpDown1.Value / (decimal)2.1)
-                g.DrawEllipse(p, beginXUp.X + (float)(numericUpDown1.Value / 8) + (float)i, (beginXDown.Y - 2- (float)numericUpDown1.Value / 8), (float)numericUpDown1.Value / 8, (float)(numericUpDown1.Value / 8));
-            float C = 0;
-            C = ((widthCadr- (float)(numericUpDown1.Value / 8)) / (CountRect+1));
-            
-           
-            int countFor = 0;
-            if (numericUpDown1.Value < 125)
-            {
-                for (float i = 0; i < ClientSize.Width; i += (float)widthCadr + (float)numericUpDown1.Value / 8 + C)
-                {
-                    if (countFor < CountRect)
-                    {
-                        g.DrawEllipse(p, i + (XCadr.X), (XCadr.Y) + 1, HeightCadr / 3, HeightCadr / 3);
-                        g.DrawLine(p, i + (XCadr.X + widthCadr / 8) - 2, XCadr.Y + 1 + HeightCadr * 2 / 3, i + (XCadr.X + widthCadr / 8) - 2, XCadr.Y + HeightCadr / 3);
-                        Point Ass = new Point((int)(i + (XCadr.X + widthCadr / 8) - 2), (int)(XCadr.Y + 1 + HeightCadr * 2 / 3));
-                        Point Leftfoot = new Point((int)(i + (XCadr.X)), XCadr.Y + (int)HeightCadr);
-                        Point Rightfoot = new Point((int)(i + (XCadr.X + widthCadr * 2 / 8 - 5)), XCadr.Y + (int)HeightCadr);
-                        g.DrawLine(p, Ass, Rightfoot);
-                        g.DrawLine(p, Ass, Leftfoot);
-                        Point neck = new Point((int)(i + (XCadr.X + widthCadr / 8)) - 2, (int)(XCadr.Y + HeightCadr / 3 + 5));
-                        Point leftHand = new Point((int)(i + (XCadr.X)), XCadr.Y + (int)(HeightCadr) - 20);
-                        Point rightHand = new Point((int)(i + (XCadr.X + widthCadr * 2 / 8 - 5)), XCadr.Y + (int)HeightCadr - 20);
-                        g.DrawLine(p, neck, leftHand);
-                        g.DrawLine(p, neck, rightHand);
-                        countFor++;
-                    }
-
-                }
-
-            }
-            else
-            {
-                for (float i = C; i < ClientSize.Width; i += (float)widthCadr + (float)numericUpDown1.Value / 8 + C)
-                {
-                    if (countFor < CountRect)
-                    {
-                        g.DrawEllipse(p, i + (XCadr.X), (XCadr.Y) + 1, HeightCadr / 3, HeightCadr / 3);
-                        g.DrawLine(p, i + (XCadr.X + widthCadr / 8) - 2, XCadr.Y + 1 + HeightCadr * 2 / 3, i + (XCadr.X + widthCadr / 8) - 2, XCadr.Y + HeightCadr / 3);
-                        Point Ass = new Point((int)(i + (XCadr.X + widthCadr / 8) - 2), (int)(XCadr.Y + 1 + HeightCadr * 2 / 3));
-                        Point Leftfoot = new Point((int)(i + (XCadr.X)), XCadr.Y + (int)HeightCadr);
-                        Point Rightfoot = new Point((int)(i + (XCadr.X + widthCadr * 2 / 8 - 5)), XCadr.Y + (int)HeightCadr);
-                        g.DrawLine(p, Ass, Rightfoot);
-                        g.DrawLine(p, Ass, Leftfoot);
-                        Point neck = new Point((int)(i + (XCadr.X + widthCadr / 8)) - 2, (int)(XCadr.Y + HeightCadr / 3 + 5));
-                        Point leftHand = new Point((int)(i + (XCadr.X)), XCadr.Y + (int)(HeightCadr) - 20);
-                        Point rightHand = new Point((int)(i + (XCadr.X + widthCadr * 2 / 8 - 5)), XCadr.Y + (int)HeightCadr - 20);
-                        g.DrawLine(p, neck, leftHand);
-                        g.DrawLine(p, neck, rightHand);
-                        countFor++;
-                    }
-
-                }
-            }
+            pictureBox1.Height = tabPage1.Height - 100;
         }
 
         private void Form1_SizeChanged(object sender, EventArgs e)
@@ -156,39 +81,8 @@ namespace ООП_11_Графика
         Color[] arrayColors;
         private void tabPage2_Paint(object sender, PaintEventArgs e)
         {
-            Graphics g = e.Graphics;
-            Random rnd = new Random();
-            Graphics ds = CreateGraphics();
-            Pen p = new Pen(Color.FromName(CB.SelectedItem.ToString()), 1);
-            float C = 360 / ((float)numericUpDown4.Value);
-            int i = 0;
-            for (float j = 0; j < 360; j += C)
-            {
-                GraphicsPath myPath = new GraphicsPath();
-                Brush br = new SolidBrush(arrayColors[i]);
-                myPath.StartFigure();
-                myPath.AddArc(new RectangleF((float)(ClientSize.Width / 2 - numericUpDown2.Value), (float)(ClientSize.Height / 2 - numericUpDown2.Value), (float)numericUpDown2.Value * 2, (float)numericUpDown2.Value * 2), (-1) * j , (-1) * C);
-                myPath.AddArc(new RectangleF((float)(ClientSize.Width / 2 - numericUpDown3.Value), (float)(ClientSize.Height / 2 - numericUpDown3.Value), (float)numericUpDown3.Value * 2, (float)numericUpDown3.Value * 2), (-1) * j, C);
-                //myPath.CloseFigure();
-                g.FillPath(br, myPath);
-                Pen outLine = new Pen(arrayColors[i], 1);
-                g.DrawPath(outLine, myPath);
-                i++;
-            }
-
-            /* Солнце
-            for (float j = 0; j < 360; j += C)
-            {
-                GraphicsPath myPath = new GraphicsPath();
-                myPath.StartFigure();
-                myPath.AddArc(new RectangleF((float)(ClientSize.Width / 2 - numericUpDown3.Value), (float)(ClientSize.Height / 2 - numericUpDown3.Value), (float)numericUpDown3.Value * 2, (float)numericUpDown3.Value * 2), j, C);
-                myPath.AddArc(new RectangleF((float)(ClientSize.Width / 2 - numericUpDown2.Value), (float)(ClientSize.Height / 2 - numericUpDown2.Value), (float)numericUpDown2.Value * 2, (float)numericUpDown2.Value * 2), j+C, C);
-                myPath.CloseFigure();
-                g.FillPath(Brushes.Black, myPath);
-                Pen outLine = new Pen(Color.Black, 2);
-                g.DrawPath(outLine, myPath);
-            }
-            */
+            //pictureBox2.Refresh();
+            pictureBox2.Width = tabPage2.Width - 140;
         }
 
         private void tabPage2_Click(object sender, EventArgs e)
@@ -202,7 +96,7 @@ namespace ООП_11_Графика
             Random rnd = new Random();
             for (int c = 0; c < arrayColors.Length; c++)
                 arrayColors[c] = Color.FromArgb(rnd.Next(1, 255), rnd.Next(1, 255), rnd.Next(1, 255));
-            Refresh();
+            pictureBox2.Refresh();
         }
         Timer timer = new Timer();
         Timer timerTwo = new Timer();
@@ -231,12 +125,12 @@ namespace ООП_11_Графика
         Pen pen = new Pen(Color.Black, 1);
         private void tabPage3_Paint(object sender, PaintEventArgs e)
         {
-            
+            pictureBox3.Height = tabPage3.Height - 66;
         }
         void timer_Tick(object sender, EventArgs e)
         {
             msec++;
-            Refresh();
+            pictureBox3.Refresh();
             neChetPaint();
             
             if (msec == 10)
@@ -257,16 +151,22 @@ namespace ООП_11_Графика
         {
             if (tabControl1.SelectedIndex == 2)
             {
-                t3 = tabPage3.CreateGraphics();
-                LengthX = 0;
+                timer.Stop();
+                timerTwo.Stop();
+
+                timer = new Timer();
+                timerTwo = new Timer();
+
+                t3 = pictureBox3.CreateGraphics();
                 timer.Interval = trackBar1.Value*4;
                 timer.Tick += new EventHandler(timer_Tick);
                 timer.Start();
                 timerTwo.Interval = trackBar1.Value;
                 timerTwo.Tick += new EventHandler(timer2_Tick);
                 timerTwo.Start();
-                LengthStep = 0;
-                LengthStep2 = 0;
+                //LengthX = 0;
+                //LengthStep = 0;
+                //LengthStep2 = 0;
                 flag = true;//если меняется, ноги движется по оси в другую сторону
                 flag2 = false;//если меняется, ноги движется по оси в другую сторону
                 flagNextLeg = false;
@@ -377,12 +277,12 @@ namespace ООП_11_Графика
 
         private void numericUpDown2_ValueChanged(object sender, EventArgs e)
         {
-            Refresh();
+           pictureBox2.Refresh();
         }
 
         private void numericUpDown3_ValueChanged(object sender, EventArgs e)
         {
-            Refresh();
+            pictureBox2.Refresh();
         }
 
         private void label6_Click(object sender, EventArgs e)
@@ -393,6 +293,138 @@ namespace ООП_11_Графика
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void pictureBox1_Paint_1(object sender, PaintEventArgs e)
+        {
+            int y = 10;
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.FromName(CB.SelectedItem.ToString()), 1);
+            Point beginXDown = new Point(0, (ClientSize.Height / 2 - 100) + (int)(numericUpDown1.Value / 2));
+            Point endXDown = new Point(ClientSize.Width, (ClientSize.Height / 2 - 100) + (int)(numericUpDown1.Value / 2));
+            Point beginXUp = new Point(0, (ClientSize.Height / 2 - 100) - (int)(numericUpDown1.Value / 2));
+            Point endXUp = new Point(ClientSize.Width, (ClientSize.Height / 2 - 100) - (int)(numericUpDown1.Value / 2));
+            g.DrawLine(p, beginXUp, endXUp);
+            g.DrawLine(p, beginXDown, endXDown);
+            //кадр
+            float widthCadr = (float)numericUpDown1.Value;
+            float HeightCadr = (float)numericUpDown1.Value * 2 / 3;
+            Point XCadr = new Point((int)(beginXUp.X + numericUpDown1.Value / 8), (int)(beginXUp.Y + numericUpDown1.Value / 6));
+            int CountRect = 0;
+            for (decimal i = 0; i < ClientSize.Width; i += numericUpDown1.Value + numericUpDown1.Value / 8)
+                if ((beginXUp.X + (float)numericUpDown1.Value / 8) + (float)i + (float)numericUpDown1.Value < ClientSize.Width - 10)
+                {
+                    g.DrawRectangle(p, (beginXUp.X + (float)numericUpDown1.Value / 8) + (float)i, (beginXUp.Y + (float)numericUpDown1.Value / 6), (float)numericUpDown1.Value, (float)numericUpDown1.Value * 2 / 3);
+                    CountRect++;
+                }
+            for (decimal i = 0; i < ClientSize.Width; i += numericUpDown1.Value / (decimal)2.1)
+                g.DrawEllipse(p, beginXUp.X + (float)(numericUpDown1.Value / 8) + (float)i, (beginXUp.Y + 2), (float)numericUpDown1.Value / 8, (float)(numericUpDown1.Value / 8));
+            for (decimal i = 0; i < ClientSize.Width; i += numericUpDown1.Value / (decimal)2.1)
+                g.DrawEllipse(p, beginXUp.X + (float)(numericUpDown1.Value / 8) + (float)i, (beginXDown.Y - 2 - (float)numericUpDown1.Value / 8), (float)numericUpDown1.Value / 8, (float)(numericUpDown1.Value / 8));
+            float C = 0;
+            C = ((widthCadr - (float)(numericUpDown1.Value / 8)) / (CountRect + 1));
+
+
+            int countFor = 0;
+            if (numericUpDown1.Value < 125)
+            {
+                for (float i = 0; i < ClientSize.Width; i += (float)widthCadr + (float)numericUpDown1.Value / 8 + C)
+                {
+                    if (countFor < CountRect)
+                    {
+                        g.DrawEllipse(p, i + (XCadr.X), (XCadr.Y) + 1, HeightCadr / 3, HeightCadr / 3);
+                        g.DrawLine(p, i + (XCadr.X + widthCadr / 8) - 2, XCadr.Y + 1 + HeightCadr * 2 / 3, i + (XCadr.X + widthCadr / 8) - 2, XCadr.Y + HeightCadr / 3);
+                        Point Ass = new Point((int)(i + (XCadr.X + widthCadr / 8) - 2), (int)(XCadr.Y + 1 + HeightCadr * 2 / 3));
+                        Point Leftfoot = new Point((int)(i + (XCadr.X)), XCadr.Y + (int)HeightCadr);
+                        Point Rightfoot = new Point((int)(i + (XCadr.X + widthCadr * 2 / 8 - 5)), XCadr.Y + (int)HeightCadr);
+                        g.DrawLine(p, Ass, Rightfoot);
+                        g.DrawLine(p, Ass, Leftfoot);
+                        Point neck = new Point((int)(i + (XCadr.X + widthCadr / 8)) - 2, (int)(XCadr.Y + HeightCadr / 3 + 5));
+                        Point leftHand = new Point((int)(i + (XCadr.X)), XCadr.Y + (int)(HeightCadr) - 20);
+                        Point rightHand = new Point((int)(i + (XCadr.X + widthCadr * 2 / 8 - 5)), XCadr.Y + (int)HeightCadr - 20);
+                        g.DrawLine(p, neck, leftHand);
+                        g.DrawLine(p, neck, rightHand);
+                        countFor++;
+                    }
+
+                }
+
+            }
+            else
+            {
+                for (float i = C; i < ClientSize.Width; i += (float)widthCadr + (float)numericUpDown1.Value / 8 + C)
+                {
+                    if (countFor < CountRect)
+                    {
+                        g.DrawEllipse(p, i + (XCadr.X), (XCadr.Y) + 1, HeightCadr / 3, HeightCadr / 3);
+                        g.DrawLine(p, i + (XCadr.X + widthCadr / 8) - 2, XCadr.Y + 1 + HeightCadr * 2 / 3, i + (XCadr.X + widthCadr / 8) - 2, XCadr.Y + HeightCadr / 3);
+                        Point Ass = new Point((int)(i + (XCadr.X + widthCadr / 8) - 2), (int)(XCadr.Y + 1 + HeightCadr * 2 / 3));
+                        Point Leftfoot = new Point((int)(i + (XCadr.X)), XCadr.Y + (int)HeightCadr);
+                        Point Rightfoot = new Point((int)(i + (XCadr.X + widthCadr * 2 / 8 - 5)), XCadr.Y + (int)HeightCadr);
+                        g.DrawLine(p, Ass, Rightfoot);
+                        g.DrawLine(p, Ass, Leftfoot);
+                        Point neck = new Point((int)(i + (XCadr.X + widthCadr / 8)) - 2, (int)(XCadr.Y + HeightCadr / 3 + 5));
+                        Point leftHand = new Point((int)(i + (XCadr.X)), XCadr.Y + (int)(HeightCadr) - 20);
+                        Point rightHand = new Point((int)(i + (XCadr.X + widthCadr * 2 / 8 - 5)), XCadr.Y + (int)HeightCadr - 20);
+                        g.DrawLine(p, neck, leftHand);
+                        g.DrawLine(p, neck, rightHand);
+                        countFor++;
+                    }
+
+                }
+            }
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Random rnd = new Random();
+            Graphics ds = CreateGraphics();
+            Pen p = new Pen(Color.FromName(CB.SelectedItem.ToString()), 1);
+            float C = 360 / ((float)numericUpDown4.Value);
+            int i = 0;
+            for (float j = 0; j < 360; j += C)
+            {
+                GraphicsPath myPath = new GraphicsPath();
+                Brush br = new SolidBrush(arrayColors[i]);
+                myPath.StartFigure();
+                myPath.AddArc(new RectangleF((float)(pictureBox2.Width / 2 - numericUpDown2.Value), (float)(pictureBox2.Height / 2 - numericUpDown2.Value), (float)numericUpDown2.Value * 2, (float)numericUpDown2.Value * 2), (-1) * j, (-1) * C);
+                myPath.AddArc(new RectangleF((float)(pictureBox2.Width / 2 - numericUpDown3.Value), (float)(pictureBox2.Height / 2 - numericUpDown3.Value), (float)numericUpDown3.Value * 2, (float)numericUpDown3.Value * 2), (-1) * j, C);
+                //myPath.CloseFigure();
+                g.FillPath(br, myPath);
+                Pen outLine = new Pen(arrayColors[i], 1);
+                g.DrawPath(outLine, myPath);
+                i++;
+            }
+
+            /* Солнце
+            for (float j = 0; j < 360; j += C)
+            {
+                GraphicsPath myPath = new GraphicsPath();
+                myPath.StartFigure();
+                myPath.AddArc(new RectangleF((float)(ClientSize.Width / 2 - numericUpDown3.Value), (float)(ClientSize.Height / 2 - numericUpDown3.Value), (float)numericUpDown3.Value * 2, (float)numericUpDown3.Value * 2), j, C);
+                myPath.AddArc(new RectangleF((float)(ClientSize.Width / 2 - numericUpDown2.Value), (float)(ClientSize.Height / 2 - numericUpDown2.Value), (float)numericUpDown2.Value * 2, (float)numericUpDown2.Value * 2), j+C, C);
+                myPath.CloseFigure();
+                g.FillPath(Brushes.Black, myPath);
+                Pen outLine = new Pen(Color.Black, 2);
+                g.DrawPath(outLine, myPath);
+            }
+            */
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tabPage3_SizeChanged(object sender, EventArgs e)
+        {
+            tabControl1_Click(sender, e);
         }
     } 
 }
